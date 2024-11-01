1d3e9c50462453607d11c4284405d21c5c49bffd
Rowel Maharjan <rowelmhj@gmail.com>
2024-11-01 16:52:58 +0545

    Configured react-roter and redux toolkit

--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
 import express from "express"
 import mongoose from "mongoose"
 import 'dotenv/config'
+import cookieParser from "cookie-parser"
+import cors from "cors";
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
 
 mongoose.connect("mongodb://localhost:27017/E-Commerce")
-// mongoose.connect(process.env.MONGO_URI)  
+    // mongoose.connect(process.env.MONGO_URI)  
     .then(() => {
         console.log("Connection Successful")
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
         })
-    }).catch(()=>{
+    }).catch((error) => {
+        console.log(error)
         console.log("Connection Failed")
     })
 
-app.get('/', async(req, res) => {
-    res.send('Hello World!')    
+app.get('/', async (req, res) => {
+    res.send('Hello World!')
 })
 
 
