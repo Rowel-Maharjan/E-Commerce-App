7be263cafc143a45e1eebff5c183e530568f4843
Rowel Maharjan <rowelmhj@gmail.com>
2024-11-02 10:15:31 +0545

    Authentication for page completed

---
+++ frontend/src/components/Authentication.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+
+const Authentication = ({ isAuthenticated, user, children }) => {
+    const location = useLocation()
+
+    if (!isAuthenticated && !(location.pathname.includes("login") || location.pathname.includes("register"))) {
+        return <Navigate to="/auth/login" />
+    }
+    if (isAuthenticated && user.role === "admin" && !(location.pathname.includes("admin"))) {
+        return <Navigate to="/admin/dashboard" />
+    }
+    if (isAuthenticated && user.role === "user" && !(location.pathname.includes("shop"))) {
+        return <Navigate to="/shop/home" />
+    }
+    return children
+
+}   
+
+export default Authentication
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,58 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-import ErrorPage from './components/ErrorPage'
 import { store } from './store/store.js';
 import { Provider } from 'react-redux'
-import AuthLayout from './components/auth/AuthLayout.jsx';
-import Login from './components/auth/Login.jsx';
-import Register from './components/auth/Register.jsx';
-import AdminLayout from './components/adminView/adminLayout/AdminLayout.jsx';
-import AdminDashboard from './components/adminView/AdminDashboard.jsx';
-import AdminFeatures from './components/adminView/AdminFeatures.jsx';
-import AdminOrders from './components/adminView/AdminOrders.jsx';
-import AdminProducts from './components/adminView/AdminProducts.jsx';
-import ShoppingLayout from './components/shoppingView/shoppingLayout/ShoppingLayout.jsx';
-import Account from './components/shoppingView/Account.jsx';
-import Checkout from './components/shoppingView/Checkout.jsx';
-import Home from './components/shoppingView/Home.jsx';
-import Listing from './components/shoppingView/Listing.jsx'; 
-import App from './App.jsx'
+import routes from './paths/routes.jsx';
 
-
-const router = createBrowserRouter([
-  {
-    path: "/auth",
-    element: <AuthLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: "login", element: <Login /> },
-      {path: "register", element: <Register /> }
-    ]
-  },
-  {
-    path: "/admin",
-    element: <AdminLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {path: "dashboard" , element: <AdminDashboard /> },
-      {path: "features", element: <AdminFeatures /> },
-      {path: "orders", element: <AdminOrders /> },
-      {path: "products", element: <AdminProducts /> }
-    ]
-  },
-  {
-    path: "/shop",
-    element: <ShoppingLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {path: "account" , element: <Account /> },
-      {path: "checkout", element: <Checkout /> },
-      {path: "home", element: <Home /> },
-      {path: "listing", element: <Listing /> }
-    ]
-  }
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
