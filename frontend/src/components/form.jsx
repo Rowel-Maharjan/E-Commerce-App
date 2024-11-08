b80cb3109312362a0c3c6ab3438e302686ab1d69
Rowel Maharjan <rowelmhj@gmail.com>
2024-11-08 11:08:01 +0545

    Form input updated

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -9,7 +9,6 @@ function CommonForm({ formControls, buttonText, onSubmit, currentEditedProduct =
 
     function handleFormSubmit(data) {
         onSubmit(data);
-        reset();
     }
 
     const [showPassword, setShowPassword] = useState(false)
@@ -25,7 +24,6 @@ function CommonForm({ formControls, buttonText, onSubmit, currentEditedProduct =
 
     function renderInputByComponentType(getControlItem) {
         const { componentType, label, name, placeholder, type, options } = getControlItem;
-        const isPassword = type === "password";
 
         switch (componentType) {
             case "input":
@@ -35,12 +33,13 @@ function CommonForm({ formControls, buttonText, onSubmit, currentEditedProduct =
                             className="border border-input px-3 py-2 text-sm placeholder:text-muted-foreground rounded-md focus:outline-gray-600 bg-[#e8f0fe] mb-1 w-full"
                             placeholder={placeholder}
                             step="any"
-                            type={isPassword && !showPassword ? "password" : "text"}
+                            type={type === "password" ? (showPassword ? "text" : "password") : type}
+                            // type={type}
                             {...register(name, {
                                 required: { value: true, message: `Please Enter ${label}` },
                             })}
                         />
-                        {isPassword && (
+                        {type === "password" && (
                             <button
                                 type="button"
                                 className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600"
